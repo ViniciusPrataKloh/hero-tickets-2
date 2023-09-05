e02ee7ac4306aa3d4697b270dffb3fd1b52db670
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-09-04 21:18:41 -0300

    Front setup

---
+++ front/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-blue': '#06038D',
+        'secondary-blue': '#61D9DE',
+        'black': '#000000',
+        'white': '#ffffff',
+
+        'custom-gray-200': '#EFEFF6',
+        'custom-gray-300': '#E6E5F0',
+        'custom-gray-400': '#E1E4F2'
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
